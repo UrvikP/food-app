86f6f09a4ee81ff047cca791609ec22343bd8bdb
UrvikP <urvik95@gmail.com>
2023-08-15 17:49:41 -0400

    Food Lovers Web App

---
+++ src/components/HeroSection.js
@@ -0,0 +1,12 @@
+export default function HeroSection(){
+    return (
+        <div className="section">
+            <div className="col">
+                <h1>Welcome to foodHub</h1>
+            </div>
+            <div className="col">
+                <h1>Have a Sausage</h1>
+            </div>
+        </div>
+    )
+}
