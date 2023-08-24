de616fd6496253f177653d9ffb6e6954f189a5d4
UrvikP <urvik95@gmail.com>
2023-08-24 13:05:39 -0400

    HeroSection, including images

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,27 @@
+import CustomImage from "./CustomImage"
 export default function HeroSection(){
+    const images = [
+        "./img/gallery/img_1.jpg",
+        "./img/gallery/img_2.jpg",
+        "./img/gallery/img_3.jpg",
+        "./img/gallery/img_4.jpg",
+        "./img/gallery/img_5.jpg",
+        "./img/gallery/img_6.jpg",
+        "./img/gallery/img_7.jpg",
+        "./img/gallery/img_8.jpg",
+        "./img/gallery/img_9.jpg"
+    ]
     return (
-        <div className="section">
-            <div className="col">
-                <h1>Welcome to foodHub</h1>
+        <div className="section hero">
+            <div className="col typography">
+                <h1 className="title">What Are We About</h1>
+                <p className="info">FoodHub is a place where you can satisfy your lust for delicious food recipes. Our service is free so what are you waiting for? Get exploring now!</p>
+                <button className="btn">Explore Now</button>
             </div>
-            <div className="col">
-                <h1>Have a Sausage</h1>
+            <div className="col gallery">
+                {images.map((src, index) => (
+                    <CustomImage key={index} imgSrc={src} pt={"90%"} />
+                ))}
             </div>
         </div>
     )
