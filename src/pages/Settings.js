e4a252f1c28639b1bdb7c4f7109e59dbe63b6dba
UrvikP <urvik95@gmail.com>
2023-09-01 15:38:17 -0400

    primary color functionality

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,11 +1,55 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Settings(){
+    const [settings, setSettings] = useState({
+        "--background-color": "#fff",
+        "--background-light": "#fff",
+        "--primary-color": "rgb(255, 0, 86)",
+        "--shadow-color": "rgba(0, 0, 0, 0.2)",
+        "--text-color": "#0A0A0A",
+        "--text-light": "#575757",
+        "--font-size": "16px",
+        "--animation-speed": 1
+    })
+
+    useEffect( () => {
+        const root = document.documentElement;
+        for(let key in settings){
+            root.style.setProperty(key, settings[key]);
+        }
+    }, [settings])
 
     const[theme, setTheme] = useState("light");
+    const themes = [
+        {
+            "--background-color": "#fff",
+            "--background-light": "#fff",
+            "--shadow-color": "rgba(0, 0, 0, 0.2)",
+            "--text-color": "#0A0A0A",
+            "--text-light": "#575757"
+        },
+        {
+            "--background-color": "rgb(29, 29, 29)",
+            "--background-light": "rgb(77, 77, 77)",
+            "--shadow-color": "rgba(0, 0, 0, 0.2)",
+            "--text-color": "#ffffff",
+            "--text-light": "#eceaea"
+        }
+    ]
+
+    function changeTheme(i){
+        const _theme = {...themes[i]}
+        setTheme(i === 0 ? "light" : "dark")
+        //update the settings
+        let _settings = {...settings}
+        for(let key in _settings){
+            _settings[key] = _theme[key]
+        }
+        setSettings(_settings)
+    }
 
     const primaryColors = [
         "rgb(255, 0, 86)",
@@ -14,7 +58,6 @@ export default function Settings(){
         "rgb(0, 200, 83)",
         "rgb(156, 39, 176)"
     ]
-
     const fontSizes = [
         {
             title: "Small",
@@ -29,7 +72,6 @@ export default function Settings(){
             value: "20px"
         }
     ]
-
     const animationSpeeds = [
         {
             title: "Slow",
@@ -44,7 +86,6 @@ export default function Settings(){
             value: .5
         }
     ]
-
     const [primaryColor, setPrimaryColor] = useState(0); // returning the index
     const [fontSize, setFontSize] = useState(1);
     const [animationSpeed, setAnimationSpeed] = useState(1);
@@ -54,14 +95,14 @@ export default function Settings(){
             <div className="section d-block">
                 <h2>Preferred Theme</h2>
                 <div className="options-container">
-                    <div className="option light">
+                    <div className="option light" onClick={() => changeTheme(0)}>
                         { theme === "light" && (
                             <div className="check">
                                 <FontAwesomeIcon icon={faCheck} />
                             </div>
                         ) }
                     </div>
-                    <div className="option dark">
+                    <div className="option dark" onClick={() => changeTheme(1)}>
                         { theme === "dark" && (
                             <div className="check">
                                 <FontAwesomeIcon icon={faCheck} />
@@ -96,7 +137,6 @@ export default function Settings(){
                                 <span><FontAwesomeIcon icon={faCheck} /></span>
                             ) }
                         </button>
-                        
                     ) ) }
                 </div>
             </div>
@@ -110,8 +150,7 @@ export default function Settings(){
                             {animationSpeed === index && (
                                 <span><FontAwesomeIcon icon={faCheck} /></span>
                             ) }
-                        </button>
-                        
+                        </button>   
                     ) ) }
                 </div>
             </div>
